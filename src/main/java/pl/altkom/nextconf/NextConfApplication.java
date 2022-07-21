6fd34100921528e6f23b177dd60dfb3e19d028de
mpietrewicz <mpietrewicz@gmail.com>
2022-07-21 11:22:00 +0200

    Inicjacja projektu

---
+++ src/main/java/pl/altkom/nextconf/NextConfApplication.java
@@ -0,0 +1,13 @@
+package pl.altkom.nextconf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextConfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextConfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/altkom/nextconf/NextConfApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.altkom.nextconf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextConfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
